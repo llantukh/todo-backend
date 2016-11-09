d9b733d78ddf85609a88af57e932e225872ae90a
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-09 16:21:29 -0500

    finished users and lists controllers, we did these in class

--- app/Http/Controllers/TodoListController.js
+++ app/Http/Controllers/TodoListController.js
@@ -4,8 +4,20 @@ const TodoList = use('app/Model/Todolist.js')
 
 class TodoListController {
 
-  * example (request, response) {
-    response.json({ message: "hello world" })
+  * create (request, response) {
+    let userId = request.param('id')
+    let data = request.only('name')
+    data.user_id = userId
+
+    let todoList = yield TodoList.create(data)
+    response.status(201).json(todoList)
+  }
+
+  * index (request, response) {
+    let userId = request.param('id')
+    let lists = yield TodoList.query().where('user_id', userId)
+
+    response.json(lists)
   }
 
 }
--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -2,7 +2,28 @@
 
 const User = use('app/Model/User.js')
 
+
 class UserController {
+
+  * create (request, response) {
+    let data = request.only('email', 'name')
+    let user = yield User.create(data)
+
+    response.json(user)
+  }
+
+  * show (request, response) {
+    let userId = request.param('id')
+    let user = yield User.find(userId)
+
+    if (user) {
+      response.json(user)
+    } else {
+      response.status(404)
+      response.json({ error: "No such user" })
+    }
+  }
+
 
 }
 
